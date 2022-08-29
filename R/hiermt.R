f737cdec639c6a75889b4ccdf8c0f29fd7c75b55
serwaabacino <serwaa.bacino@gmail.com>
2022-08-29 15:33:19 -0600

    Added rcorrmatrix to the example to make better dendrograms.
    Removed method as an argument

--- R/hiermt.R
+++ R/hiermt.R
@@ -7,13 +7,14 @@
 #' @param cor_mat A Q*Q square matrix of the correlations between the p-values or test statistics.
 #' @param global_test Global test to use when testing the intersection of hypotheses. This should be one of "bonferonni", "ghc", "gbj".
 #' @param alpha Probability of Type I error. Default is set to 1.
-#' @param method Multiple testing adjustment method, a character string. "Meinshausen's method", "Inheritance procedure"
 #' @export
 #'
 #' @import stats
 #' @import MASS
 #' @import ggplot2
 #' @import ggdendro
+#' @import GBJ
+#' @importFrom clusterGeneration rcorrmatrix
 #' @importFrom magrittr %>%
 #' @importFrom data.table data.table :=
 #' @importFrom purrr map map2 map_dbl map2_dbl map_lgl
@@ -24,20 +25,19 @@
 #' @examples
 #' set.seed(1)
 #' Q <- 50
-#' test_stats <- rnorm(50, mean = c(rep(0, 25), rep(3, 25)))
+#' test_stats <- rnorm(Q, mean = c(rep(0, Q/2), rep(3, Q/2)))
 #' pvalues <- 2*pnorm(sort(-abs(test_stats)))
-#' cor_mat <- matrix(data= 0.3, nrow=Q, ncol=Q)
-#' diag(cor_mat) <- 1
-#' hiermt(pvalues, test_stats, cor_mat, global_test="bonferroni", alpha = 0.05, method=NULL)
+#' cor_mat <- clusterGeneration::rcorrmatrix(Q)
+#' hiermt(pvalues, test_stats, cor_mat, global_test="bonferroni", alpha = 0.05)
 
 
 
-hiermt <- function(pvalues, test_stats, cor_mat, global_test, alpha=1, method=NULL){
+hiermt <- function(pvalues, test_stats, cor_mat, global_test = "ghc", alpha=1){
   global_tests <- c("bonferroni", "ghc", "gbj")
   if (!global_test %in% global_tests)stop("Global test arguement should be one of \"bonferonni\", \"ghc\", \"gbj\".")
 
   Q <- length(pvalues)
-  if(length(test_stats)!=Q )stop("P-value vector and test statistic vector have differing lengths.")
+  if(length(test_stats)!= Q)stop("P-value vector and test statistic vector have differing lengths.")
   if(any(dim(cor_mat) != c(Q,Q)))stop("Pairwise correlation matrix is of the wrong size.")
 
   hc <- hclust(as.dist(sqrt(2*(1-abs(cor_mat)))),method="ward.D2")
@@ -50,16 +50,34 @@ hiermt <- function(pvalues, test_stats, cor_mat, global_test, alpha=1, method=NU
     color_label <- ggendplot <- line_color <- show_label <-
     x <- xend <- y <- yend <- NULL
 
+  calculate_global_pvalue <- function(p, t, corr, test){
+    global_pvalue <- 1
+    if(test == "bonferroni"){
+      global_pvalue = min(p*length(p))
+    }
+    else if (length(p) == 1){
+      global_pvalue = p
+    }
+    else if (test ==  "ghc"){
+      global_pvalue = GHC(t, corr)$GHC_pvalue
+    }
+    else {
+      global_pvalue = GBJ(t, corr)$GBJ_pvalue
+    }
+    return(global_pvalue)
+  }
+
   attr <- data.table(S = c(1:nn),
                      nodes = partition_leaves(dend),
-                     isnodeleaf = as.integer(which_leaf(dend)))%>%mutate(
+                     isnodeleaf = as.integer(
+                       which_leaf(dend)))%>%mutate(
                        descendants = map(nodes,function(y)map_lgl(nodes,function(x)all(x%in%y))%>%which()),
                        ancestors = map(nodes, function(x)which_node(dend, x, max_id=FALSE)))%>%mutate(
                          parent = map(ancestors, function(y)nth(y,-2)))%>%mutate(
                            sibling = map2(parent, S, function(x,y)setdiff(which(parent==x),y)))%>%mutate(
                              issiblingleaf = map(sibling, function(x)isnodeleaf[x]))%>%mutate(
                                adjustment = map2_dbl(nodes, issiblingleaf, function(x,y) max((Q/(length(x)+y)),1)))%>%mutate(
-                                 node_pvalue = map_dbl(nodes, function(x)min(min(pvalues[x]*length(nodes)),1)))%>%mutate(
+                                 node_pvalue = map_dbl(nodes, function(x)min(calculate_global_pvalue(pvalues[x],test_stats[x],fsubset(cor_mat,x,x),global_test),1)))%>%mutate(
                                    adj_pvalue = map2_dbl(node_pvalue, adjustment, function(x,y) min(x*y,1)))%>%mutate(
                                      hadj_pvalue = map_dbl(ancestors, function(x) max(adj_pvalue[x])))
   attr_leaf <- attr[attr$isnodeleaf==1]
@@ -77,16 +95,19 @@ hiermt <- function(pvalues, test_stats, cor_mat, global_test, alpha=1, method=NU
               hjust ="outward",
               nudge_x = 0.15,
               nudge_y = -0.01,
-              size = ifelse(Q<=10,2,ifelse(Q<=50,1.5,1)))+
+              size = ifelse(Q<=10,3,ifelse(Q<=50,2.5,ifelse(Q<=100,2,1))))+
     geom_label(data = leaf_labels_df,
                aes(x=X1, y=X2, label= show_label),
                size = ifelse(Q<=10,2,ifelse(Q<=50,1.5,1)),
                na.rm=TRUE)+
     scale_color_manual(values = c("#bfbfbf",
                                   "#000000"))+
-    scale_y_continuous(limits = c(-0.8,4.5))+
+    #scale_y_continuous(limits = c(-0.8,4.5))+
     theme_void()+
+    coord_flip()+
     theme(legend.position="none")
-  return(list(dend=ggdend_plot,dat=attr_leaf,order=hc$order))
+  leaves <- data.frame(index = attr_leaf$nodes, h_adj_pvalue = attr_leaf$hadj_pvalue)
+  return(list(dend=ggdend_plot,leaves = leaves, all_nodes = attr))
 }
+
 
