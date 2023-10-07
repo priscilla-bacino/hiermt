deb0c581523bf30a7500c987fa076b7c7a4687bf
priscilla-bacino <101588393+priscilla-bacino@users.noreply.github.com>
2023-10-07 08:49:25 -0600

    Added cachexia data to package

---
+++ R/data-cachexia.R
@@ -0,0 +1,15 @@
+#' Human Cachexia
+#'
+#' Named metabolite concentrations of 77 urine samples from two groups of cancer patients
+#' measured by 1H NMR. Group 1 - Cachexic; Group 2 - Control.
+#'
+#' @format A data frame with 77 rows and 65 columns.
+#'
+#' @source \emph{Metaboanalyst 5.0}
+#' https://www.xialab.ca/api/download/metaboanalyst/human_cachexia.csv
+#'
+#' @references Eisner, R., Stretch, C., Eastman, T. et al. Learning to predict
+#' cancer-associated skeletal muscle wasting from 1H-NMR profiles of urinary
+#' metabolites. Metabolomics 7, 25–34 (2011). https://doi.org/10.1007/s11306-010-0232-9
+#'
+"cachexia"
